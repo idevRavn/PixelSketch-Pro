028d6f4541d8b273b2ad299274099d67950abe77
Sanu Kumar <idevravn@gmail.com>
2024-02-05 16:50:49 +0530

    Update index.js
    Added basic function to get divs, color divs with black and rainbow colors with toggle buttons

---
+++ index.js
@@ -0,0 +1,75 @@
+const containerDiv = document.querySelector(".container");
+const colorPicker = document.querySelector("#color-picker");
+const rainbowBtn = document.querySelector("#rainbow");
+const eraserBtn = document.querySelector("#eraser");
+const gridSize = document.querySelector("#grid-size");
+const resetBtn = document.querySelector("#reset");
+const showGrid = document.querySelector("#show-grid");
+const gridSizeText = document.querySelector(".message");
+const itemsDiv = document.querySelectorAll(".items");
+
+let isRainbowSelected = false;
+let isEraserSelected = false;
+let currentColor = `#000`;
+const sizeOfPad = 500;
+let isDrawing = false;
+let isGrid = false;
+
+rainbowBtn.addEventListener("click", () => {
+  isRainbowSelected = !isRainbowSelected;
+
+  if (isRainbowSelected) {
+    rainbowBtn.classList.add("selected");
+    rainbowBtn.textContent = "Toggled Rainbow";
+  } else {
+    rainbowBtn.classList.remove("selected");
+    rainbowBtn.textContent = "Toggle Rainbow";
+  }
+});
+
+function colorDiv(e) {
+  if (e.type === "mousedown") {
+    isDrawing = true;
+    e.target.style.backgroundColor = isRainbowSelected
+      ? getRandomColor()
+      : currentColor;
+  } else if (e.type === "mouseover" && isDrawing) {
+    e.target.style.backgroundColor = isRainbowSelected
+      ? getRandomColor()
+      : currentColor;
+  } else isDrawing = false;
+}
+
+function getRandomColor() {
+  const redVal = Math.floor(Math.random() * 256);
+  const greenVal = Math.floor(Math.random() * 256);
+  const blueVal = Math.floor(Math.random() * 256);
+
+  return `rgb(${redVal}, ${greenVal}, ${blueVal})`;
+}
+
+function getDivItems() {
+  containerDiv.textContent = "";
+  gridNum = gridSize.value;
+  gridSizeText.textContent = `${gridNum} x ${gridNum}`;
+  for (let i = 0; i < gridNum * gridNum; i++) {
+    const box = document.createElement("div");
+
+    box.style.width = `${sizeOfPad / gridNum}px`;
+    box.style.height = `${sizeOfPad / gridNum}px`;
+    box.classList.add("items");
+
+    containerDiv.appendChild(box);
+    box.addEventListener("mousedown", (e) => {
+      colorDiv(e);
+    });
+    box.addEventListener("mouseover", (e) => {
+      colorDiv(e);
+    });
+    box.addEventListener("mouseup", (e) => {
+      colorDiv(e);
+    });
+  }
+}
+
+getDivItems();
